ee2a0d1c046b442ba34c1eb59703ff1bd7e3246d
Petr.Hlavenka <Petr.Hlavenka@admosphere.cz>
2019-01-01 21:37:29 +0100

    Oprava prejmenovani projektu

---
+++ Pool/Matika 2.0/Matika/Configurations/MatikaConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using Mediaresearch.Framework.Utilities.Configuration;
+
+namespace Matika.Configurations
+{
+    public class MatikaConfiguration : ConfigBase
+    {
+        public override string GetConfigName() => nameof(MatikaConfiguration);
+
+        [ConfigurationProperty(nameof(Test), IsRequired = true)]
+        public int Test => (int)this["Test"];
+
+    }
+}
--- Pool/Matika 2.0/Shell/Installers/ShellInstaller.cs
+++ Pool/Matika 2.0/Shell/Installers/ShellInstaller.cs
@@ -3,6 +3,8 @@ using Castle.MicroKernel.Registration;
 using Castle.MicroKernel.SubSystems.Configuration;
 using Castle.Windsor;
 using Matika;
+using Matika.Configurations;
+using Mediaresearch.Framework.Utilities.Configuration;
 
 namespace Shell.Installers
 {
@@ -10,8 +12,12 @@ namespace Shell.Installers
     {
         public void Install(IWindsorContainer container, IConfigurationStore store)
         {
+            container.Register(Component.For<IConfigurationProvider>().ImplementedBy<ConfigurationProvider>());
             container.Register(Component.For<IWindowManager>().ImplementedBy<WindowManager>());
             container.Register(Component.For<MainViewModel>());
+
+            //var provider = container.Resolve<IConfigurationProvider>();
+            //var test = provider.GetConfig<MatikaConfiguration>().Test;
         }
     }
 }
