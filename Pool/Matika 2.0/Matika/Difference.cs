f086bf95a1d64d8ddb01542a0855cfc75c37a788
Petr.Hlavenka <Petr.Hlavenka@admosphere.cz>
2019-01-01 16:31:48 +0100

    Matika oprava prejmenovani

---
+++ Pool/Matika 2.0/Matika/Difference.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Matika_2._0
+{
+    public class Difference : Example
+    {
+        public Difference(int diffuculty)
+        {
+            var maximum = diffuculty * 10;
+            var first = new Random().Next(maximum + 1);
+            var second = new Random().Next(10 + 1);
+
+            Result = first - second;
+
+            Task = string.Join(" ", first, Sign, second, EqualSign);
+        }
+
+        private static string Sign => " - ";
+    }
+}
---
+++ Pool/Matika 2.0/Matika/MainView.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using Matika;
+using Matika_2._0;
+
+namespace Matika
+{
+    /// <summary>
+    ///     Interaction logic for MainView.xaml
+    /// </summary>
+    public partial class MainView : Window
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+
+
+        private void MainView_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is MainViewModel dc)
+            {
+                dc.ResultTextBox = ResultTextBox;
+            }
+        }
+    }
+}
