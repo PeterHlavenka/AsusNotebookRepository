011eb206f4c1467eacba610f5a2dcdb0272b812d
Petr.Hlavenka <Petr.Hlavenka@admosphere.cz>
2019-01-01 21:04:31 +0100

    Matika - Castle Windsor

--- Pool/Matika 2.0/Shell/Bootstrapper.cs
+++ Pool/Matika 2.0/Shell/Bootstrapper.cs
@@ -1,16 +1,33 @@
 ﻿using System.Collections.Generic;
 using System.Reflection;
+using System.Windows;
 using Caliburn.Micro;
+using Castle.Windsor;
 using Matika;
+using Shell.Installers;
 
 
 namespace Shell
 {
     public class Bootstrapper : Bootstrapper<MainViewModel>
     {
+        private IWindsorContainer m_container;
+
         protected override IEnumerable<Assembly> SelectAssemblies()
         {
             return new[] {Assembly.GetAssembly(typeof(MainViewModel))};
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {           
+            m_container = new WindsorContainer();
+
+            m_container.Install(new ShellInstaller());
+
+            var root = m_container.Resolve<MainViewModel>();
+            var manager = m_container.Resolve<IWindowManager>();
+            manager.ShowDialog(root);
+            Application.Shutdown();
         }
     }
 }
---
+++ Pool/Matika 2.0/Shell/Installers/ShellInstaller.cs
@@ -0,0 +1,17 @@
+﻿using Caliburn.Micro;
+using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+using Matika;
+
+namespace Shell.Installers
+{
+    public class ShellInstaller: IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container.Register(Component.For<IWindowManager>().ImplementedBy<WindowManager>());
+            container.Register(Component.For<MainViewModel>());
+        }
+    }
+}
