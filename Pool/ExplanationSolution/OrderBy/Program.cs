b86bfe5c5e22d9bc70c57a1e52cc365f9671b1a9
Petr.Hlavenka <Petr.Hlavenka@admosphere.cz>
2019-05-19 12:03:36 +0200

    asdf

---
+++ Pool/ExplanationSolution/OrderBy/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OrderBy
+{ // NA MULTISELECT COMBO BOXU STACI ZAPNOUT STARTSWITH = TRUE V XAMLU
+    internal class Program
+    {
+        private static string Input = "N";
+
+        private static void Main(string[] args)
+        {      
+            List<Person> persons = new List<Person>() { new Person(1, "N"), new Person(2, "N1"), new Person(3, "Tonda"), new Person(2, "1N"), new Person(2, "!N"),
+                new Person(2, "N!"), new Person(2, "NN"), new Person(2, "N 101"), new Person(2, "N 103.4 HiTech RedLine"), new Person(2, "N 144G"),
+                new Person(2, "N 113 HiTech"), new Person(2, "N!KTO tour 2015"), new Person(2, "n&sluno"), new Person(2, "N 6X2 NMX"), new Person(2, "N 97 MINI") };
+
+            //Console.WriteLine("persons = persons.OrderBy(d => d.Name).ToList();");
+            //persons = persons.OrderBy(d => d.Name).ToList();
+            //Write(persons);
+            //Console.WriteLine();
+
+            //// vubec nefunguje pro order
+            //Console.WriteLine("persons = persons.OrderBy(d => d.Name.StartsWith(Input)).ToList();");
+            //persons = persons.OrderBy(d => d.Name.ToLower().StartsWith(Input.ToLower())).ToList();
+            //Write(persons);
+            //Console.WriteLine();
+
+            //Console.WriteLine("persons = persons.OrderBy(d => d.Name.Length == Input.Length).ToList();");
+            //persons = persons.OrderBy(d => d.Name.Length == Input.Length).ToList();
+            //Write(persons);
+            //Console.WriteLine();
+
+            //// Tady je dulezite dat to na ToLower - je to CaseSensitive
+            //Console.WriteLine("persons = persons.OrderBy(d => d.Name.IndexOf(Input)).ToList();");
+            //persons = persons.OrderBy(d => d.Name.ToLower().IndexOf(Input.ToLower(), StringComparison.Ordinal)).ThenBy(x => x.Name.Length).ToList();
+            //Write(persons);
+            Console.WriteLine();
+
+            //// Tady je dulezite dat to pripadne na ToLower - je to CaseSensitive
+            //Console.WriteLine("persons = persons.Where(d => d.Name.Contains(Input)).OrderBy(d => d.Name.IndexOf(Input, StringComparison.Ordinal)).ThenBy(x => x.Name.Length).ToList();");
+            //persons = persons.Where(d => d.Name.Contains(Input)).OrderBy(d => d.Name.IndexOf(Input, StringComparison.Ordinal)).ThenBy(x => x.Name.Length).ToList();
+            //Write(persons);
+            //Console.WriteLine();
+
+            // Tady je dulezite dat to pripadne na ToLower - je to CaseSensitive, ToString returns Name
+            Console.WriteLine("persons = persons.Where(d => d.Name.ToLower().Contains(Input.ToLower())).OrderBy(d => d.Name.ToLower().IndexOf(Input.ToLower(), StringComparison.Ordinal)).ThenBy(d => d.Name).ToList();");
+            persons = persons.Where(d => d.Name.ToLower().Contains(Input.ToLower())).OrderBy(d => d.ToString().ToLower().IndexOf(Input.ToLower(), StringComparison.Ordinal)).ThenBy(d => d.ToString()).ToList();
+            Write(persons);
+            Console.WriteLine();
+
+            Console.ReadLine();
+        }
+
+        public class Person
+        {
+            public Person(int id, string name)
+            {
+                Id = id;
+                Name = name;
+            }
+
+            public int Id { get; set; }
+
+            public string Name { get; set; }
+
+            public override string ToString()
+            {
+                return Name;
+            }
+        }
+
+        public static void Write(List<Person> persons)
+        {
+            persons.ForEach(d => Console.WriteLine(d.Name));
+        }
+    }
+}
---
+++ Pool/ExplanationSolution/StringComparer/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+
+namespace StringComparer
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            List<string> words = new List<string> {"N", "!Neco", "!!Heel", "Heel", "! N 101", "BOmba"};
+            List<Person> persons = new List<Person>() {new Person(1, "Petr"), new Person(2, "Michal"), new Person(3, "Tonda")}; 
+
+            
+            // 1) Pokud mam tridu, ktera dedi od IComparer<T> a umi porovnat objekty muzu ji pouzit takto:   (bue pouzito porovnavani v teto tride:)
+            var comparer = new CaseSensitiveComparer();
+            words.Sort(comparer);
+
+            // 2) Osoby muzu radit podle kriteria, ktere jim dam do lambdy:
+           // persons.Sort((Person x, Person y) => {x.CompareTo(y)}); nejak tak..
+
+
+            // Sort metoda musi vedet jak ma prvky seradit. Pomoci lambdy vytvorim comparer, ktery predam metode Sort a ta bude radit podle nej.
+            // Metoda CompareTo je schopna porovnat vsechny tridy, ktere implementuji rozhrani IComparable. Napr. Integer.
+            words.Sort((x, y) =>
+            {
+                if (Char.IsLetterOrDigit(x[0]))
+                {
+                    if (!Char.IsLetterOrDigit(y[0]))
+                    {
+                        // x is a letter/digit and y is not, override regular CompareTo
+                        return -1;
+                    }
+                }
+                else if (Char.IsLetterOrDigit(y[0]))
+                {
+                    // y is a letter/digit and x is not, override regular CompareTo
+                    return 1;
+                }
+                return x.CompareTo(y);
+            });
+
+            // Protoze jde o stringy a ty jsou comparable, muzu udelat toto:
+            words.Sort();
+
+            // pokud by slo o objekty
+            words.Sort((x, y) => x.CompareTo(y));
+
+
+            foreach (var s in words)
+            {
+                Console.WriteLine(s + "\n");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+
+    // 1) Muzu mit tridu, ktera mi porovnava objekty. 
+    public class CaseSensitiveComparer : IComparer<string>
+    {
+        public int Compare(string x, string y)
+        {
+            return string.Compare(x, y, StringComparison.Ordinal);
+        }
+    }
+
+    // 2) Komplexni typ - metoda Sort nevi podle ceho ma porovnat - podle Id nebo podle Name?
+    public class Person: IComparable<Person>
+    {
+        public Person(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+
+      // Prvni moznost je, ze by trida implementovala IComparable a v metode CompareTo by rekla, jak se tyto typy radi:
+      public int CompareTo(Person other)
+      {
+         return this.Name.CompareTo(other.Name); // porovnavej podle jmena
+         // return this.Id.CompareTo(other.Id);  // porovnavej podle Id
+      }
+    }
+}
