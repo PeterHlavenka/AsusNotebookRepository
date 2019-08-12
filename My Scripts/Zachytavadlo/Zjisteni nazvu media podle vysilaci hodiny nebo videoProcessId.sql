270e3e3c4567f83d000544d95bbacc125c39866f
Petr.Hlavenka <Petr.Hlavenka@admosphere.cz>
2019-08-12 07:27:39 +0200

    doi

---
+++ My Scripts/Zachytavadlo/Zjisteni nazvu media podle vysilaci hodiny nebo videoProcessId.sql
@@ -0,0 +1,13 @@
+
+-- Na zaklade casu videa najde videoprocess, k nemu skrze tabulku TvMedium priradi medium a pres tabulku MediumVersion zjisti nazev media.
+
+SELECT TOP 100  mv.Name, vps.Name,  *
+FROM Creative.VideoProcess vp 
+JOIN media.TvMedium tm ON vp.TvStorageChannelId = tm.TvStorageChannelId
+JOIN media.MediumVersion mv ON mv.MediumId = tm.Id
+JOIN Creative.VideoProcessStatus vps ON vp.VideoProcessStatusId = vps.Id
+WHERE vp.VideoDateTime >= '2019-07-19 06:00:00' AND vp.VideoDateTime < '2019-07-19 07:00:00' 
+AND mv.Name = 'Nova'
+
+SELECT TOP 100 *
+FROM Creative.VideoProcessStatus vps
