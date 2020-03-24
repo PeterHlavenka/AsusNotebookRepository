1cfccfd1e866c058c52927b02f6d6b3f38f2780e
Petr.Hlavenka <Petr.Hlavenka@admosphere.cz>
2020-03-24 23:59:30 +0100

    triggery na big settings

--- Pool/Matika 2.0/Gui/Settings/BigNumbersSettingsViewModel.cs
+++ Pool/Matika 2.0/Gui/Settings/BigNumbersSettingsViewModel.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace Matika.Settings
 {
@@ -19,5 +21,33 @@ namespace Matika.Settings
         public bool CanWholeThousand => FirstNumberSize > 1000 && SecondNumberSize > 1000;
         public bool WholeThousands { get; set; }
         public bool Overlaps { get; set; }
+
+        public void FirstTextBoxTextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (sender is TextBox tb)
+            {
+                var text = Regex.Replace(tb.Text, @"\s+", string.Empty);
+
+                if ( int.TryParse(text, out var number))
+                {
+                    FirstNumberSize = number;
+                    NotifyOfPropertyChange(() =>(FirstNumberSize));
+                }
+            }
+        }
+
+        public void SecondTextBoxTextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (sender is TextBox tb)
+            {
+                var text = Regex.Replace(tb.Text, @"\s+", string.Empty);
+
+                if (int.TryParse(text, out var number))
+                {
+                    SecondNumberSize = number;
+                    NotifyOfPropertyChange(() => (SecondNumberSize));
+                }
+            }
+        }
     }
 }
