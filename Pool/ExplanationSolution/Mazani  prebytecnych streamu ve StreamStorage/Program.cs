7ce031067e8ca2f7504d40911c6714153f752cb1
PeterHlavenka <hlavickyy@gmail.com>
2021-02-11 09:16:31 +0100

    Run external program + opravy nugetu

---
+++ Pool/ExplanationSolution/Mazani  prebytecnych streamu ve StreamStorage/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using log4net;
+
+namespace Mazani__prebytecnych_streamu_ve_StreamStorage
+{
+    internal class Program
+    {
+        private static readonly ILog m_log = LogManager.GetLogger(MethodBase.GetCurrentMethod()?.DeclaringType?.Name);
+
+        private static void Main(string[] args)
+        {
+            var path = "d:\\Cache\\MIR.Media.Catching\\StreamStorage\\";
+
+            var directory = Directory.CreateDirectory(path);
+
+            var filesToRemove = directory.EnumerateFiles().OrderByDescending(d => d.LastWriteTime).Skip(3).ToList();
+
+            try
+            {
+                foreach (var fileInfo in filesToRemove)
+                {
+                    m_log.Debug($@"Deleting file: {fileInfo.FullName}");
+                    fileInfo.Delete();
+                }
+            }
+            catch (Exception e)
+            {
+                m_log.Error(@"Unable to clean cache", e);
+            }
+        }
+    }
+}
---
+++ Pool/ExplanationSolution/log4net_explanation a run external exe program/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using log4net;
+
+namespace log4net_explanation
+{
+    static class Program
+    {
+        private static readonly ILog m_log = LogManager.GetLogger(MethodBase.GetCurrentMethod()?.DeclaringType?.Name);
+
+        private static void Main(string[] args)
+        {
+            m_log.Debug($@"Starting cache maintenance for StreamStorage");
+            Process externalProcess = new Process {StartInfo = {FileName = "CacheMaintenance/Mazani  prebytecnych streamu ve StreamStorage.exe"}};
+            //externalProcess.StartInfo.WindowStyle = ProcessWindowStyle.Maximized;
+            externalProcess.Start();
+            externalProcess.WaitForExit();
+        }
+    }
+}
