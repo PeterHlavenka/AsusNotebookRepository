77bdfaef835c4e050972626d00d0e6ec686b12bf
PeterHlavenka <hlavickyy@gmail.com>
2021-02-24 07:46:31 +0100

    StringBuilderAppendNullTest

---
+++ Pool/ExplanationSolution/StringBuilderAppendNullTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace StringBuilderAppendNullTest
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string necoNull = null;
+
+            var toolTip = new StringBuilder()
+                .Append("mediumId: ")
+                .Append(1234)
+                .Append(" ").Append("name")
+                .Append(": ")
+                .Append(necoNull)
+                .ToString();
+
+            Console.WriteLine(toolTip);
+            Console.ReadLine();
+        }
+    }
+}
