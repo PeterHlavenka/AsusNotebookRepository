b7b3b43c646963deb7e3c065c1998d833190004a
PeterHlavenka <hlavickyy@gmail.com>
2021-04-23 14:59:37 +0200

    progress circle

---
+++ Pool/ExplanationSolution/ChytiVyjimkuAzObCatch/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ChytiVyjimkuAzObCatch
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+
+                try   // v prostrednim try neni catch, takze vyjimka jede dal.
+                {
+
+                    try
+                    {
+                        throw new Exception();
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                        throw;
+                    }
+                }
+                finally
+                {
+                    Console.WriteLine();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception jsem chytil az tady.  Mazec");
+                
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Pool/ExplanationSolution/PokracujeForeeachVeKteremBylaVyhozenaVyjimka/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace PokracujeForeeachVeKteremBylaVyhozenaVyjimka
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            try
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine(i);
+
+                    if (i == 3)
+                    {
+                        throw new Exception();
+                    }
+                
+                }
+
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Console.ReadLine();
+            }
+        }
+    }
+}
