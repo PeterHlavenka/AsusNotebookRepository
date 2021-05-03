c6f4226a4b413f4c4b7e2b5486791a62d5885f78
PeterHlavenka <hlavickyy@gmail.com>
2021-05-03 14:38:31 +0200

    RadCartesianChart

--- Pool/ExplanationSolution/RadCartesianChartTest/BindedSeriesChartDatacontext.cs
+++ Pool/ExplanationSolution/RadCartesianChartTest/BindedSeriesChartDatacontext.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Windows;
 using Caliburn.Micro;
 using Telerik.Windows.Controls;
@@ -9,24 +8,21 @@ namespace RadCartesianChartTest
 {
     public class BindedSeriesChartDatacontext : Screen
     {
+        private readonly Random rnd = new Random();
+
         public BindedSeriesChartDatacontext(RadCartesianChart chart)
         {
-
             Chart = chart;
             Data = new List<Product>();
             GetData();
-            
-            // chart.MaxWidth = 1000;
-            // var neco = chart.MaxWidth / Data.Count;
-            //  chart.Zoom = new Size(neco, 1);
 
             AddEmptyValuesToData();
-            
-             chart.Width = Math.Min( Data.Count * 30, 500);
-            
-             chart.Zoom = new Size(Zoom, 1);
+
+            chart.Width = Math.Min(Data.Count * 30, 500);
+
+            chart.Zoom = new Size(Zoom, 1);
         }
-        
+
         public RadCartesianChart Chart { get; set; }
 
         private int PocetMistNaOseX => 20;
@@ -34,27 +30,26 @@ namespace RadCartesianChartTest
 
         private int Zoom => PocetNacitanychDatDoGrafu / PocetMistNaOseX;
 
+        public List<Product> Data { get; set; }
+
         private void AddEmptyValuesToData()
         {
-            for (int i = Data.Count; i < PocetMistNaOseX; i++)
+            for (var i = Data.Count; i < PocetMistNaOseX; i++)
             {
-                Product product = new Product {Name = ""+i, QuantitySold = 0};
+                var product = new Product {Name = "" + i, QuantitySold = 0};
                 Data.Add(product);
             }
+
             NotifyOfPropertyChange(nameof(Data));
         }
 
-        public List<Product> Data { get; set; }
-        Random rnd = new Random();
-
         private void GetData()
         {
-            for (int i = 0; i < PocetNacitanychDatDoGrafu; i++)
+            for (var i = 0; i < PocetNacitanychDatDoGrafu; i++)
             {
                 var product = new Product {Name = "Product " + i, QuantitySold = rnd.Next(10, 99)};
                 Data.Add(product);
             }
-            
         }
 
         public void Reload()
@@ -62,25 +57,17 @@ namespace RadCartesianChartTest
             Data = new List<Product>();
             GetData();
             AddEmptyValuesToData();
-            Chart.Width = Math.Min( Data.Count * 30, 500);
+            Chart.Width = Math.Min(Data.Count * 30, 500);
             Chart.Zoom = new Size(Zoom, 1);
             NotifyOfPropertyChange(nameof(Data));
         }
     }
 
 
-
     public class Product
     {
-        public string Name
-        {
-            get;
-            set;
-        }
-        public double QuantitySold
-        {
-            get;
-            set;
-        }
+        public string Name { get; set; }
+
+        public double QuantitySold { get; set; }
     }
 }
