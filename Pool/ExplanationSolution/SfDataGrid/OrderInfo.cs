8cde678dc56df64a85177d73a35cae8b108430ad
PeterHlavenka <hlavickyy@gmail.com>
2021-12-17 14:09:39 +0100

    syncfusion

---
+++ Pool/ExplanationSolution/SfDataGrid/OrderInfo.cs
@@ -0,0 +1,24 @@
+﻿namespace SfDataGrid
+{
+    public class OrderInfo
+    {
+        public OrderInfo(int orderId, string customerName, string country, string customerId, string shipCity)
+        {
+            OrderId = orderId;
+            CustomerName = customerName;
+            Country = country;
+            CustomerId = customerId;
+            ShipCity = shipCity;
+        }
+
+        public int OrderId { get; set; }
+
+        public string CustomerId { get; set; }
+
+        public string CustomerName { get; set; }
+
+        public string Country { get; set; }
+
+        public string ShipCity { get; set; }
+    }
+}
