290a82dfa7f3767f56bdaa18924508fd38364074
PeterHlavenka <hlavickyy@gmail.com>
2021-12-22 21:44:13 +0100

    works

--- AndroidSolution/SmsReader/SmsReader.Android/MyBroadcastReceiver.cs
+++ AndroidSolution/SmsReader/SmsReader.Android/MyBroadcastReceiver.cs
@@ -1,83 +1,46 @@
-﻿// #region
-//
-// using Android.App;
-// using Android.Content;
-// using Android.Telephony;
-// using Android.Widget;
-// using Java.Lang;
-//
-// #endregion
-//
-// namespace SmsReader.Android
-// {
-//     [BroadcastReceiver]
-//     [IntentFilter(new[] {"android.provider.Telephony.SMS_RECEIVED"}, Priority = (int) IntentFilterPriority.HighPriority)]
-//     public class MyBroadcastReceiver : BroadcastReceiver
-//     {
-//         private string m_message = string.Empty;
-//         private string m_address = string.Empty;
-//         private const string Name = "pdus";
-//
-//         public override void OnReceive(Context context, Intent intent)
-//         {
-//             if (!intent.HasExtra(Name) || intent.Extras == null) return;
-//             
-//             var smsArray = (Object[]) intent.Extras.Get(Name);
-//
-//             if (smsArray == null) return;
-//             
-//             foreach (var item in smsArray)
-//             {
-//                 var sms = SmsMessage.CreateFromPdu((byte[]) item, null);
-//
-//                 if (sms == null) continue;
-//                 
-//                 // m_address = sms.OriginatingAddress;
-//                 m_message = sms.MessageBody;
-//
-//                 // if (m_message != null && m_message.Contains("critical:"))
-//                 // {
-//                     Toast.MakeText(context, m_message, ToastLength.Long)?.Show();
-//
-//                     var playService = new PlaySoundService();
-//                     playService.PlaySystemSound();
-//                 // }
-//             }
-//         }
-//     }
-// }
+﻿#region
 
 using Android.App;
 using Android.Content;
 using Android.Telephony;
 using Android.Widget;
+using Java.Lang;
+
+#endregion
 
 namespace SmsReader.Android
 {
     [BroadcastReceiver]
-    [IntentFilter(new[]{"android.provider.Telephony.SMS_RECEIVED"}, Priority = (int)IntentFilterPriority.HighPriority)]
+    [IntentFilter(new[] {"android.provider.Telephony.SMS_RECEIVED"}, Priority = (int) IntentFilterPriority.HighPriority)]
     public class MyBroadcastReceiver : BroadcastReceiver
     {
-        public static readonly string INTENT_ACTION = "android.provider.Telephony.SMS_RECEIVED";
-        protected string message, address = "";
-        
+        private string m_message = string.Empty;
+        private string m_address = string.Empty;
+        private const string Name = "pdus";
+
         public override void OnReceive(Context context, Intent intent)
         {
-            if (intent.HasExtra("pdus"))
-            {
-                var smsArray = (Java.Lang.Object[])intent.Extras.Get("pdus");
+            if (!intent.HasExtra(Name) || intent.Extras == null) return;
+            
+            var smsArray = (Object[]) intent.Extras.Get(Name);
 
-                foreach (var item in smsArray)
-                {
-                    var sms = SmsMessage.CreateFromPdu((byte[]) item);
-                    address = sms.OriginatingAddress;
-                    message = sms.MessageBody;
-                    
-                    Toast.MakeText(context, "Number" + address + "Message:"+ message,ToastLength.Short)?.Show();
-                    
-                    var playService = new PlaySoundService();
-                    playService.PlaySystemSound();
-                }
+            if (smsArray == null) return;
+            
+            foreach (var item in smsArray)
+            {
+                var sms = SmsMessage.CreateFromPdu((byte[]) item);
+
+                if (sms == null) continue;
+                
+                // m_address = sms.OriginatingAddress;
+                m_message = sms.MessageBody;
+
+                if (m_message == null || !m_message.Contains("critical:")) continue;
+                
+                Toast.MakeText(context, m_message, ToastLength.Long)?.Show();
+
+                var playService = new PlaySoundService();
+                playService.PlaySystemSound();
             }
         }
     }
