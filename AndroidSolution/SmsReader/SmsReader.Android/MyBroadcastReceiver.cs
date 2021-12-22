8830b2d434c75bec232bbfa63277ee9d407877c4
PeterHlavenka <hlavickyy@gmail.com>
2021-12-22 20:24:46 +0100

    refak

--- AndroidSolution/SmsReader/SmsReader.Android/MyBroadcastReceiver.cs
+++ AndroidSolution/SmsReader/SmsReader.Android/MyBroadcastReceiver.cs
@@ -1,33 +1,43 @@
-﻿using Android.App;
+﻿#region
+
+using Android.App;
 using Android.Content;
 using Android.Telephony;
 using Android.Widget;
+using Java.Lang;
+
+#endregion
 
 namespace SmsReader.Android
 {
     [BroadcastReceiver]
-    [IntentFilter(new[]{"android.provider.Telephony.SMS_RECEIVED"}, Priority = (int)IntentFilterPriority.HighPriority)]
+    [IntentFilter(new[] {"android.provider.Telephony.SMS_RECEIVED"}, Priority = (int) IntentFilterPriority.HighPriority)]
     public class MyBroadcastReceiver : BroadcastReceiver
     {
-        public static readonly string INTENT_ACTION = "android.provider.Telephony.SMS_RECEIVED";
-        protected string message, address = "";
-        
+        private string m_message;
+        private string m_address = "";
+        private const string Name = "pdus";
+
         public override void OnReceive(Context context, Intent intent)
         {
-            if (intent.HasExtra("pdus"))
-            {
-                var smsArray = (Java.Lang.Object[])intent.Extras.Get("pdus");
+            if (!intent.HasExtra(Name) || intent.Extras == null) return;
+            
+            var smsArray = (Object[]) intent.Extras.Get(Name);
 
-                foreach (var item in smsArray)
+            if (smsArray == null) return;
+            
+            foreach (var item in smsArray)
+            {
+                var sms = SmsMessage.CreateFromPdu((byte[]) item, null);
+
+                if (sms != null)
                 {
-                    var sms = SmsMessage.CreateFromPdu((byte[]) item);
-                    address = sms.OriginatingAddress;
-                    message = sms.MessageBody;
-                    
-                    Toast.MakeText(context, "Number" + address + "Message:"+ message,ToastLength.Short).Show();
+                    // m_address = sms.OriginatingAddress;
+                    m_message = sms.MessageBody;
                 }
+
+                Toast.MakeText(context, m_message, ToastLength.Long)?.Show();
             }
-           // Toast.MakeText(context, "Received intent!", ToastLength.Short)?.Show();
         }
     }
 }
