8a51c5a73ae23577bd4c8a39499903fdd4b3df76
PeterHlavenka <hlavickyy@gmail.com>
2021-12-22 21:32:07 +0100

    opraveno a hraje

--- AndroidSolution/SmsReader/SmsReader.Android/MyBroadcastReceiver.cs
+++ AndroidSolution/SmsReader/SmsReader.Android/MyBroadcastReceiver.cs
@@ -1,47 +1,83 @@
-﻿#region
+﻿// #region
+//
+// using Android.App;
+// using Android.Content;
+// using Android.Telephony;
+// using Android.Widget;
+// using Java.Lang;
+//
+// #endregion
+//
+// namespace SmsReader.Android
+// {
+//     [BroadcastReceiver]
+//     [IntentFilter(new[] {"android.provider.Telephony.SMS_RECEIVED"}, Priority = (int) IntentFilterPriority.HighPriority)]
+//     public class MyBroadcastReceiver : BroadcastReceiver
+//     {
+//         private string m_message = string.Empty;
+//         private string m_address = string.Empty;
+//         private const string Name = "pdus";
+//
+//         public override void OnReceive(Context context, Intent intent)
+//         {
+//             if (!intent.HasExtra(Name) || intent.Extras == null) return;
+//             
+//             var smsArray = (Object[]) intent.Extras.Get(Name);
+//
+//             if (smsArray == null) return;
+//             
+//             foreach (var item in smsArray)
+//             {
+//                 var sms = SmsMessage.CreateFromPdu((byte[]) item, null);
+//
+//                 if (sms == null) continue;
+//                 
+//                 // m_address = sms.OriginatingAddress;
+//                 m_message = sms.MessageBody;
+//
+//                 // if (m_message != null && m_message.Contains("critical:"))
+//                 // {
+//                     Toast.MakeText(context, m_message, ToastLength.Long)?.Show();
+//
+//                     var playService = new PlaySoundService();
+//                     playService.PlaySystemSound();
+//                 // }
+//             }
+//         }
+//     }
+// }
 
 using Android.App;
 using Android.Content;
 using Android.Telephony;
 using Android.Widget;
-using Java.Lang;
-
-#endregion
 
 namespace SmsReader.Android
 {
     [BroadcastReceiver]
-    [IntentFilter(new[] {"android.provider.Telephony.SMS_RECEIVED"}, Priority = (int) IntentFilterPriority.HighPriority)]
+    [IntentFilter(new[]{"android.provider.Telephony.SMS_RECEIVED"}, Priority = (int)IntentFilterPriority.HighPriority)]
     public class MyBroadcastReceiver : BroadcastReceiver
     {
-        private string m_message = string.Empty;
-        private string m_address = string.Empty;
-        private const string Name = "pdus";
-
+        public static readonly string INTENT_ACTION = "android.provider.Telephony.SMS_RECEIVED";
+        protected string message, address = "";
+        
         public override void OnReceive(Context context, Intent intent)
         {
-            if (!intent.HasExtra(Name) || intent.Extras == null) return;
-            
-            var smsArray = (Object[]) intent.Extras.Get(Name);
-
-            if (smsArray == null) return;
-            
-            foreach (var item in smsArray)
+            if (intent.HasExtra("pdus"))
             {
-                var sms = SmsMessage.CreateFromPdu((byte[]) item, null);
+                var smsArray = (Java.Lang.Object[])intent.Extras.Get("pdus");
 
-                if (sms == null) continue;
-                
-                // m_address = sms.OriginatingAddress;
-                m_message = sms.MessageBody;
-
-                // if (m_message != null && m_message.Contains("critical:"))
-                // {
-                    Toast.MakeText(context, m_message, ToastLength.Long)?.Show();
-
+                foreach (var item in smsArray)
+                {
+                    var sms = SmsMessage.CreateFromPdu((byte[]) item);
+                    address = sms.OriginatingAddress;
+                    message = sms.MessageBody;
+                    
+                    Toast.MakeText(context, "Number" + address + "Message:"+ message,ToastLength.Short)?.Show();
+                    
                     var playService = new PlaySoundService();
                     playService.PlaySystemSound();
-                // }
+                }
             }
         }
     }
