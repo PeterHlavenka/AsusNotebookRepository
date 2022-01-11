d6bf913216aad6b8105ad7d118e98511bc36baa2
PeterHlavenka <hlavickyy@gmail.com>
2022-01-11 21:45:20 +0100

    neco

--- Pool/ExplanationSolution/change-themes-at-runtime-using-skinmanager-master/MainWindow.xaml.cs
+++ Pool/ExplanationSolution/change-themes-at-runtime-using-skinmanager-master/MainWindow.xaml.cs
@@ -7,18 +7,7 @@
 #endregion
 using Syncfusion.Windows.Shared;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using Syncfusion.SfSkinManager;
 
 namespace DataGrid_Themes
