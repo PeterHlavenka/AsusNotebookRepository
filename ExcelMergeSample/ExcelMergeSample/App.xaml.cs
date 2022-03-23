7ec8fd9b61bce1a366819c21ae2a0a0ce65ea3b7
PeterHlavenka <hlavickyy@gmail.com>
2022-03-23 12:22:54 +0100

    neco je nic aneb merge sample na rowning monthspro syncfusny

---
+++ ExcelMergeSample/ExcelMergeSample/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿#region
+
+using Syncfusion.Licensing;
+
+#endregion
+
+namespace ExcelMergeSample
+{
+    /// <summary>
+    ///     Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        public App()
+        {
+            SyncfusionLicenseProvider.RegisterLicense("NTg4MzYyQDMxMzkyZTM0MmUzMFltY3pWc1l6d3ExVkZzNitrbTJOMGdKenFtdGtzb3lCcnRIK29yTXFXRHM9");
+        }
+    }
+}
---
+++ ExcelMergeSample/ExcelMergeSample/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿#region
+
+using System.Windows;
+using Syncfusion.XlsIO;
+
+#endregion
+
+namespace ExcelMergeSample
+{
+    /// <summary>
+    ///     Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            using (var excelEngine = new ExcelEngine())
+            {
+                //Instantiate the Excel application object
+                var application = excelEngine.Excel;
+
+                //Set the default application version
+                application.DefaultVersion = ExcelVersion.Excel2016;
+
+                //Load the existing Excel workbook into IWorkbook
+                var destinationWorkbook = application.Workbooks.Open("Rovning_months.xlsx");
+
+                var workbookToAdd = application.Workbooks.Open("Rovning_weeks.xlsx");
+
+                destinationWorkbook.Worksheets.AddCopy(workbookToAdd.Worksheets);
+
+                //Save the Excel document
+                destinationWorkbook.SaveAs("Output.xlsx");
+            }
+        }
+    }
+}
