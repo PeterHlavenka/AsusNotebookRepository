79956f12f7bba6ed6817de0c0c9e2c1809d93868
PeterHlavenka <hlavickyy@gmail.com>
2022-11-03 06:48:34 +0100

    add project , custom theme is inside in folder

---
+++ Pool/WPF_Ribbon/WPF_Ribbon/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Syncfusion.SfSkinManager;
+using Syncfusion.Windows.Tools.Controls;
+
+namespace WPF_Ribbon
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : RibbonWindow
+    {
+        public MainWindow()
+        {
+            SfThemeRegistrator.RegisterTheme(VisualStyles.Office2019Colorful);
+           
+            InitializeComponent();
+            SfThemeRegistrator.SetVisualStyle(this);
+            SfThemeRegistrator.SetTheme(ContentGrid);   //Nejde !!
+            // SfThemeRegistrator.SetTheme(this);          //Nejde !!
+            // SfThemeRegistrator.SetTheme(Button);
+        }
+        
+    }
+}
---
+++ Pool/WPF_Ribbon/WPF_Ribbon/WPF_Ribbon/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace WPF_Ribbon
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow 
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
