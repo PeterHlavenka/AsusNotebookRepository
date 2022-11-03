89a1c53ee38bd1313d5a2a628f89a9fdedb50c48
PeterHlavenka <hlavickyy@gmail.com>
2022-11-03 09:18:08 +0100

    verze 20.3.0.47 nefunkcni pro odeslani

---
+++ Pool/WPF_Ribbon_20.3.0.47/WPF_Ribbon/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Syncfusion.SfSkinManager;
+
+namespace WPF_Ribbon
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow 
+    {
+        public MainWindow()
+        {
+            SfThemeRegistrator.RegisterTheme(VisualStyles.Office2019Colorful);
+           
+            InitializeComponent();
+           // SfThemeRegistrator.SetVisualStyle(this);
+
+            SfThemeRegistrator.SetTheme(this);          // Not possible: 
+             SfThemeRegistrator.SetTheme(Button);
+        }
+        
+    }
+}
