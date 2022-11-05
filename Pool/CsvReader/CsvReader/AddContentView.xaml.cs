3a7831a9bc3fa06c860cfdc422d31657e11c54fc
PeterHlavenka <hlavickyy@gmail.com>
2022-11-05 09:25:59 +0100

    export do excelu

--- Pool/CsvReader/CsvReader/AddContentView.xaml.cs
+++ Pool/CsvReader/CsvReader/AddContentView.xaml.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Controls;
-using Syncfusion.Windows.ComponentModel;
-using Syncfusion.Windows.Controls.Grid;
+using Syncfusion.UI.Xaml.Grid.Converter;
 
 namespace CsvReader;
 
@@ -13,9 +12,6 @@ public partial class AddContentView : UserControl
     public AddContentView()
     {
         InitializeComponent();
-        GridControl.Model.TableStyle.ReadOnly = false;
-        GridControl.Model.RowCount = 35;
-        GridControl.Model.ColumnCount = 4;
 
         for (int i = 1; i < 101; i++)
         {
@@ -23,50 +19,16 @@ public partial class AddContentView : UserControl
         }
 
         m_list.First().Cz = "ceskyNazev";
-        
-        GridControl.Model.QueryCellInfo += (s, e) =>
-        {
-            if (e.Style.RowIndex > 0 && e.Style.ColumnIndex > 0)
-            {
-                var obj = m_list.ElementAt(e.Style.RowIndex - 1);
-                if (e.Cell.ColumnIndex == 1)
-                {
-                    e.Style.CellValue = obj.Cz;
-                }
-                if (e.Cell.ColumnIndex == 2)
-                {
-                    e.Style.CellValue = obj.En;
-                }
-                if (e.Cell.ColumnIndex == 3)
-                {
-                    e.Style.CellValue = obj.De;
-                }
-               // e.Style.CellValue = string.Format("R{0}C{1}", e.Cell.RowIndex, e.Cell.ColumnIndex);
-            }
-                
-        };
-        //this.GridControl.Model.ColumnWidths[0] = 80;
 
-        GridControl.Model.Options.ColumnSizer = GridControlLengthUnitType.AutoWithLastColumnFill;
+        SfDataGrid.ItemsSource = m_list;
     }
 
-    private void GridControl_OnCurrentCellEditingComplete(object sender, SyncfusionRoutedEventArgs args)
+    public void Save()
     {
-        if (sender is GridControl gridControl)
-        {
-            var neco = gridControl.CurrentCell.CellRowColumnIndex;
-            var obj = m_list.ElementAt(neco.RowIndex - 1);
-
-            if (neco.ColumnIndex == 1)
-            {
-                var str = obj.Cz;
-            }
-        }
-       
-    }
-
-    private void GridControl_OnSelectionChanging(object sender, GridSelectionChangingEventArgs e)
-    {
-        var test = sender;
+        var options = new ExcelExportingOptions();
+        var excelEngine = SfDataGrid.ExportToExcel(SfDataGrid.View, options);
+        var workBook = excelEngine.Excel.Workbooks[0];
+        workBook.SaveAs("Sample.xlsx");
+        // workBook.SaveAs("Sample.csv", ",");
     }
 }
--- Pool/CsvReader/CsvReader/MainWindow.xaml.cs
+++ Pool/CsvReader/CsvReader/MainWindow.xaml.cs
@@ -1,11 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using Syncfusion.Windows.Controls.Grid.Converter;
 
 namespace CsvReader;
 
@@ -15,27 +13,15 @@ namespace CsvReader;
 public partial class MainWindow : Window, INotifyPropertyChanged
 {
     private Visibility m_addControlVisibility;
-    private ImageSource m_buttonImageSource;
+    private ImageSource m_buttonImageSource = new BitmapImage(new Uri(".\\Icons\\Ok.png", UriKind.Relative));
+    private TrainingView m_trainingView;
+    private AddContentView m_addContentView;
 
     public MainWindow()
     {
         InitializeComponent();
         DataContext = this;
-
-        ButtonImageSource = new BitmapImage(new Uri(".\\Icons\\Ok.png", UriKind.Relative));
-        
-        Words = new List<TranslatedObject>();
-
-        for (int i = 1; i < 100; i++)
-        {
-            Words.Add(new TranslatedObject(i.ToString(), string.Empty, String.Empty, String.Empty));
-        }
-        
-        // DataGrid.ItemsSource = Words;
     }
-
-    public TrainingView TrainingView { get; set; }
-    public AddContentView AddContentView { get; set; }
 
     public Visibility AddControlVisibility
     {
@@ -51,7 +37,7 @@ public partial class MainWindow : Window, INotifyPropertyChanged
     public ImageSource ButtonImageSource
     {
         get => m_buttonImageSource;
-        set
+        private set
         {
             if (Equals(value, m_buttonImageSource)) return;
             m_buttonImageSource = value;
@@ -59,60 +45,31 @@ public partial class MainWindow : Window, INotifyPropertyChanged
         }
     }
 
-    private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
-    {
-        if (AddControlVisibility == Visibility.Visible)
-        {
-            Save();
-        }
-        
-        AddControlVisibility = AddControlVisibility == Visibility.Visible ? Visibility.Collapsed : Visibility.Visible;
-        ButtonImageSource = AddControlVisibility == Visibility.Visible? 
-            new BitmapImage(new Uri(".\\Icons\\Ok.png", UriKind.Relative)):
-            new BitmapImage(new Uri(".\\Icons\\Plus.png", UriKind.Relative));
-    }
-
-    public void Save()
-    {
-        // need to install 	Syncfusion.GridExcelExport.Wpf
-        AddContentView.GridControl.Model.ExportToCSV("Sample.csv");
-        // if (NameTextBox.Text == String.Empty)
-        // {
-        //     MessageBox.Show("Vyplňte název souboru");
-        // }
-        //
-        // string fileName = string.Concat(NameTextBox.Text, ".csv");
-        // string path = Directory.GetCurrentDirectory();
-        //
-        // var together = string.Concat(path, "\\");
-        // together = string.Concat(together, fileName);
-        //
-        // using (var writer = new StreamWriter(together))
-        // using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
-        // {
-        //     csv.WriteRecords(Words);
-        // }
-    }
-
-
-    public List<TranslatedObject> Words { get; set; }
-
-
-
     public event PropertyChangedEventHandler? PropertyChanged;
 
+    // Zmeni Image na buttonu a pro AddContentView zavola Save
+    private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+    {
+        if (AddControlVisibility == Visibility.Visible) m_addContentView.Save();
+
+        AddControlVisibility = AddControlVisibility == Visibility.Visible ? Visibility.Collapsed : Visibility.Visible;
+        ButtonImageSource = AddControlVisibility == Visibility.Visible ? 
+            new BitmapImage(new Uri(".\\Icons\\Ok.png", UriKind.Relative)) : 
+            new BitmapImage(new Uri(".\\Icons\\Plus.png", UriKind.Relative));
+    }
+    
     protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
     {
         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 
-
+    // Priradi kontrolky z xamlu do propert zde.
     private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
     {
         if (sender is MainWindow mainWindow)
         {
-            TrainingView = mainWindow.TrainingView;
-            AddContentView = mainWindow.AddContentControl;
+            m_trainingView = mainWindow.TrainingControl;
+            m_addContentView = mainWindow.AddContentControl;
         }
     }
 }
