35193ae1cf62fce97872afec26229d07c2e735d0
PeterHlavenka <hlavickyy@gmail.com>
2023-01-20 08:00:42 +0100

    rw

---
+++ Pool/XlsxReader/XlsxReaderNetCoreProject/Helper.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using Syncfusion.XlsIO;
+
+namespace XlsxReaderNetCoreProject;
+
+public class Helper
+{
+    public void Save(IWorkbook workbook)
+    {
+        using var excelEngine = new ExcelEngine();
+        //Set the version of the workbook
+        workbook.Version = ExcelVersion.Excel2013;
+
+        //Save the workbook as stream
+        using var outputStream = new MemoryStream();
+        workbook.SaveAs(outputStream);
+    }
+
+    public IWorkbook OpenFile(ExcelEngine excelEngine, string inputFileName)
+    {
+        //Initialize application
+        var app = excelEngine.Excel;
+
+        //Set default application version as Xlsx
+        app.DefaultVersion = ExcelVersion.Xlsx;
+
+        //Load the file into stream
+        using var inputStream = new FileStream(inputFileName, FileMode.Open);
+
+        //Open existing Excel workbook from the specified location
+        var workbook = app.Workbooks.Open(inputStream, ExcelOpenType.Automatic);
+
+        return workbook;
+    }
+}
---
+++ Pool/XlsxReader/XlsxReaderNetCoreProject/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.IO;
+using Syncfusion.XlsIO;
+using XlsxReaderNetCoreProject;
+
+var helper = new Helper();
+string directory = Directory.GetCurrentDirectory();
+directory = directory[..^17];
+string name = "Sample.xlsx";
+string path = Path.Combine(directory, name);
+
+using var excelEngine = new ExcelEngine();
+var workbook = helper.OpenFile(excelEngine, path);
+
+//Access the first worksheet
+var worksheet = workbook.Worksheets[0];
+        
+//Access the used range of the Excel file
+var usedRange = worksheet.UsedRange;
+var lastRow = usedRange.LastRow;
+var lastColumn = usedRange.LastColumn;
+            
+//Iterate the cells in the used range and print the cell values
+for (var row = 1; row <= lastRow; row++)
+{
+    for (var col = 1; col <= lastColumn; col++)
+    {
+        Console.WriteLine(worksheet[row, col].Value);
+    }
+}
+
+//Save workbook
+string outputFileName = Path.Combine(directory, "output.xlsx");
+using var fileStream = new FileStream(outputFileName, FileMode.Create);
+workbook.SaveAs(fileStream);
