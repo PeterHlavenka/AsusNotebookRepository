e4010fe37ed1fe626e96ecf7b7caffd4189224cf
PeterHlavenka <peter.hlavenka.consultant@nielsen.com>
2023-02-15 17:07:50 +0100

    client

---
+++ Pool/ExplanationSolution/ProcessCommunicationClient/Client48/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Client48
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ Pool/ExplanationSolution/ProcessCommunicationClient/Client48/Client.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Client48;
+
+/// <summary>
+///     Interaction logic for Client.xaml
+/// </summary>
+public partial class Client
+{
+    public Client()
+    {
+        InitializeComponent();
+        ClientTextBox.Text = "After initialize";
+
+        StartHost();
+    }
+
+    private async void StartHost()
+    {
+        ClientTextBox.Text = "Starting";
+
+        var host = Host.CreateDefaultBuilder(null)
+            .ConfigureServices(services =>
+            {
+                services.AddHostedService<Worker>();
+                services.AddSingleton(ClientTextBox);
+            })
+            .Build();
+
+        await host.RunAsync();
+    }
+}
