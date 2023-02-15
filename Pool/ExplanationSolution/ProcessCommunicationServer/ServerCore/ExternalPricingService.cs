f67f95b76b1c75a7665343d0d7c6f777df0bbd36
PeterHlavenka <peter.hlavenka.consultant@nielsen.com>
2023-02-15 10:58:23 +0100

    Nechapu security okno uz zas nevyskakuje..   No zde je navod na pridani certifikat a trusted aplikace v .netu6

--- Pool/ExplanationSolution/ProcessCommunicationServer/ServerCore/ExternalPricingService.cs
+++ Pool/ExplanationSolution/ProcessCommunicationServer/ServerCore/ExternalPricingService.cs
@@ -1,4 +1,7 @@
 ﻿using System.Diagnostics;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Principal;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
@@ -17,6 +20,31 @@ public class ExternalPricingService : BackgroundService
     private Task LaunchDotNet48WpfApplication()
     {
         var pathToDotNet48WpfApplication = @"d:\AsusNotebookRepository\Pool\ExplanationSolution\ProcessCommunicationClient\Client\bin\Debug\Client.exe";
+        //
+        // var currentUser = WindowsIdentity.GetCurrent();
+        // var trustStatus = new X509ChainPolicy();
+        // trustStatus.VerificationFlags = X509VerificationFlags.AllowUnknownCertificateAuthority;
+        // trustStatus.RevocationFlag = X509RevocationFlag.EntireChain;
+        // trustStatus.ExtraStore.Add(new X509Certificate2("C:\\MyCert.pfx"));
+        // var chain = new X509Chain();
+        // chain.ChainPolicy = trustStatus;
+        // var isValid = chain.Build(new X509Certificate2(pathToDotNet48WpfApplication));
+        // if (isValid)
+        // {
+        //     var rule = new System.Security.AccessControl.FileSystemAccessRule(
+        //         currentUser.Name,
+        //         System.Security.AccessControl.FileSystemRights.FullControl,
+        //         System.Security.AccessControl.AccessControlType.Allow);
+        //     var info = new System.IO.FileInfo("C:\\MyApp.exe");
+        //     var security = info.GetAccessControl();
+        //     security.AddAccessRule(rule);
+        //     info.SetAccessControl(security);
+        // }
+        
+        
+        
+        
+        
         var startInfo = new ProcessStartInfo(pathToDotNet48WpfApplication)
         {
             UseShellExecute = true,
