d290dd21ef03552d331726b756e724b06ecb0b71
PeterHlavenka <peter.hlavenka.consultant@nielsen.com>
2023-08-24 06:46:45 +0200

    ej

--- Pool/ExplanationSolution/SyncfusionGridControl/SyncfusionGridControl/MainWindow.xaml.cs
+++ Pool/ExplanationSolution/SyncfusionGridControl/SyncfusionGridControl/MainWindow.xaml.cs
@@ -36,11 +36,15 @@ namespace SyncfusionGridControl
 
 
             // 3.Naplnim bunky explicitne (Syncfusni indexuji explicitne)  [row, column]:
-            GridControl.Model[1, 1].CellValue = "KFC_2razdvatrictyri";
+            GridControl.Model[0, 1].CellValue = "1234567890123456789nadpis";
+            GridControl.Model[0, 1].HorizontalAlignment = HorizontalAlignment.Right;
+            GridControl.Model[0, 1].TextWrapping = TextWrapping.Wrap;
+                
+            GridControl.Model[1, 1].CellValue = "1234";
             GridControl.Model[2, 1].CellValue = "KFC_1";
 
-            GridControl.Model[1, 2].CellValue = "";
-            GridControl.Model[2, 2].CellValue = "";
+            GridControl.Model[1, 2].CellValue = "ahoj";
+            GridControl.Model[2, 2].CellValue = "nazdar";
 
 
             GridControl.PrepareRenderCell += GridPrepareRenderCell; // obarvim bunku
@@ -50,6 +54,11 @@ namespace SyncfusionGridControl
             {
                 e.Style.CellValue = $"{e.Cell.RowIndex}/{e.Cell.ColumnIndex}";
             }
+
+
+            GridControl.Model.HeaderStyle.TextWrapping = TextWrapping.Wrap;
+            GridControl.Model.HeaderStyle.TextTrimming = TextTrimming.None;
+            // GridControl.Model.ResizeRowsToFit(GridRangeInfo.Row(0), GridResizeToFitOptions.None);
         }
 
         private void GridPrepareRenderCell(object sender, GridPrepareRenderCellEventArgs e)
@@ -74,19 +83,42 @@ namespace SyncfusionGridControl
             var workbook = excelEngine.Excel.Workbooks.Add();
 
             var gridModel = GridControl.Model;
+            gridModel.HeaderStyle.TextWrapping = TextWrapping.Wrap;
             gridModel.ExportToExcel(GridRangeInfo.Cells(0, 0, gridModel.RowCount - 1, gridModel.ColumnCount - 1), workbook.Worksheets[0], workbook.Worksheets[0].Range[1, 1], exportingHandler: null);
 
             // GridControl.Model.ExportToExcel("Sample.xlsx", ExcelVersion.Excel2016);
 
+            
+            
             if (autoFit)
             {
                 var mySheet = workbook.Worksheets[0];
                 var range = mySheet.UsedRange.IntersectWith(mySheet.Range[1, 1, 100, 255]);
                 
+               
+                range.IndentLevel = 5; // posunu se o 5, ale sirka sloupce je mi jedno - kus stringu nebude videt..
                 range.AutofitColumns();
-                range.IndentLevel = 5; // posunu se o 5, ale sirka sloupce je mi jedno - kus stringu nebude videt.. 
+            }
+            else
+            {
+                // Bug 55022: Detailní plán - chybně nastavená šířka sloupce při exportu do excelu a pdf
+                // Testuji zde, jestli se velikost fontu orizne pokud presahuje urcenou sirku sloupce. Result : orizne se, pokud ve vedlejsi bunce je nejaky text.
+                
+                //Creating a new style with cell back color, fill pattern and font attribute
+                IStyle style = workbook.Styles.Add("NewStyle");
+                //style.FillPattern = ExcelPattern.DarkUpwardDiagonal;
+                style.Font.Bold = true;
+                style.Font.Size = 30;
+                workbook.Worksheets[0].Range["B2"].CellStyle = style;
+                workbook.Worksheets[0].Range["B2"].ColumnWidth = 15;
+                
+                foreach (var column in workbook.Worksheets[0].Columns)
+                {
+                    column.ColumnWidth = 15;
+                }
             }
 
+     
 
             workbook.SaveAs("Sample.xlsx");
             excelEngine.ThrowNotSavedOnDestroy = false;
