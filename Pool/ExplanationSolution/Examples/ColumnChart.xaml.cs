fbe528ad9bb3d37ce66e4a1efa6853c6dab695b9
PeterHlavenka <peter.hlavenka.consultant@nielsen.com>
2024-01-19 13:22:18 +0100

    SfChart posunute sloupce s convertorem

--- Pool/ExplanationSolution/Examples/ColumnChart.xaml.cs
+++ Pool/ExplanationSolution/Examples/ColumnChart.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Windows.Controls;
 using System.Windows.Data;
 
@@ -16,12 +17,52 @@ public partial class ColumnChart : UserControl
             new() { Name = "Steve", Height = 160 },
             new() { Name = "Joel", Height = 182 }
         };
+        
         InitializeComponent();
         ColumnSeries.ItemsSource = Data;
+        //CalculateMargin();
        
     }
 
     public List<Person> Data { get; set; }
+
+    private void AddButton_Click(object sender, System.Windows.RoutedEventArgs e)
+    {
+        var list = Data.ToList();
+        list.Add(new Person { Name = "Added", Height = 150 });
+
+        Data = list;
+        ColumnSeries.ItemsSource = Data;
+        //CalculateMargin();
+    }
+
+    private void RemoveButton_Click(object sender, System.Windows.RoutedEventArgs e)
+    {
+        var list = Data.ToList();
+        list.RemoveAt(Data.Count - 1);
+
+        Data = list;
+        ColumnSeries.ItemsSource = Data;
+        //CalculateMargin();
+    }
+
+    private void CalculateMargin()
+    {
+        var left = 0d;
+        var chartWidth = 1000;
+        var columnCount = Data.Count;
+
+        if(columnCount > 0)
+        {
+            left = (chartWidth / columnCount) / 2;
+            // remove 14% of left value
+            left = -(left - (left * 0.14));
+        }
+
+
+
+        ColumnSeries.Margin = new System.Windows.Thickness(left, 0, 0, 0);
+    }
 }
 
 public class Person
---
+++ Pool/ExplanationSolution/Examples/OffsetConverter.cs
@@ -0,0 +1,70 @@
+﻿using Syncfusion.UI.Xaml.Charts;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace SfChartFontInReview
+{
+    class OffsetConverter : IMultiValueConverter
+    {
+
+        // kdyby to byl jen IValueConverter:
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double left = 0;
+            if (double.TryParse(value?.ToString(), out var columnCount))
+            {
+                var chartWidth = 1000;
+
+                if (columnCount > 0)
+                {
+                    left = (chartWidth / columnCount) / 2;
+                    // remove 14% of left value
+                    left = -(left - (left * 0.14));
+                }
+            }
+
+            return new Thickness(left, 0, 0, 0);
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        //Multi:
+
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            double left = 0;           
+            if (double.TryParse(values[0]?.ToString(), out var columnCount) && double.TryParse(values[1]?.ToString(), out var plotOffset))
+            {
+                var chartWidth = 1000;
+
+                if (columnCount > 0)
+                {
+                    left = (chartWidth / columnCount) / 2;
+                    // remove 14% of left value
+                    left = -(left - (left * 0.14));
+                }
+            }
+           
+                return new System.Windows.Thickness(left, 0, 0, 0);
+
+     
+
+        }
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+}
