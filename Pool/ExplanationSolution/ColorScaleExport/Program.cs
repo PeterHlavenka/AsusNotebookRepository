6e923a42b99a8e08ddaf122b97a399200cfc989f
PeterHlavenka <peter.hlavenka.consultant@nielsen.com>
2024-06-21 09:29:12 +0200

    ColorScale

---
+++ Pool/ExplanationSolution/ColorScaleExport/Program.cs
@@ -0,0 +1,52 @@
+﻿// Create a worksheet.        
+
+using System.Diagnostics;
+using Syncfusion.Licensing;
+using Syncfusion.XlsIO;
+
+SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NHaF1cWWhIfEx1RHxQdld5ZFRHallYTnNWUj0eQnxTdEFjX31fcHRWQWBcVE11Ww==");
+var excelEngine = new ExcelEngine();
+var application = excelEngine.Excel;
+application.DefaultVersion = ExcelVersion.Excel2013;
+var workbook = application.Workbooks.Create(1);
+var worksheet = workbook.Worksheets[0];
+
+// Load data to Apply Conditional Formatting.
+worksheet["A1"].Text = "ColorScale";
+worksheet["A2"].Number = 10;
+worksheet["A3"].Number = 20;
+worksheet["A4"].Number = 30;
+worksheet["A5"].Number = 40;
+worksheet["A6"].Number = 50;
+worksheet["A7"].Number = 60;
+worksheet["A8"].Number = 70;
+worksheet["A9"].Number = 80;
+worksheet["A10"].Number = 90;
+worksheet["A11"].Number = 100;
+
+// Create instance of IConditonalFormat and IConditionalFormats.
+var formats = worksheet["A2:A11"].ConditionalFormats;
+var format = formats.AddCondition();
+
+// Set FormatType as ColorScale.
+format.FormatType = ExcelCFType.ColorScale;
+var colorScale = format.ColorScale;
+
+// Set 3 as count for color scale
+colorScale.SetConditionCount(3);
+
+// Change Threshold value for 2nd object in Critera list.
+colorScale.Criteria[1].Value = "20";
+
+// Save and Dispose.
+var outputStream = new FileStream("ConditionalFormat.xlsx", FileMode.Create, FileAccess.Write);
+workbook.SaveAs(outputStream);
+workbook.Close();
+excelEngine.Dispose();
+
+var process = new Process();
+process.StartInfo = new ProcessStartInfo("ConditionalFormat.xlsx")
+{
+    UseShellExecute = true
+};
+process.Start();
---
+++ Pool/ExplanationSolution/ExportToExcelR1C1/Program.cs
@@ -0,0 +1,68 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+﻿using System.Diagnostics;
+using Syncfusion.Drawing;
+using Syncfusion.Licensing;
+using Syncfusion.XlsIO;
+
+namespace ExportToExcelR1C1;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NHaF1cWWhIfEx1RHxQdld5ZFRHallYTnNWUj0eQnxTdEFjX31fcHRWQWBcVE11Ww==");
+
+        using var excelEngine = new ExcelEngine();
+        var application = excelEngine.Excel;
+        application.DefaultVersion = ExcelVersion.Xlsx;
+        var workbook = application.Workbooks.Create(1);
+        var worksheet = workbook.Worksheets[0];
+        
+        GenerateTimeSpan(worksheet, "B", TimeSpan.FromMinutes(183));
+        GenerateTimeSpan(worksheet, "D", TimeSpan.FromMinutes(20));
+        
+        IConditionalFormats conditionalFormats = worksheet.Range["B10:B17"].ConditionalFormats;
+        IConditionalFormat conditionalFormat = conditionalFormats.AddCondition();
+        conditionalFormat.FormatType = ExcelCFType.ColorScale;
+        IColorScale colorScale = conditionalFormat.ColorScale;
+
+        //Sets 3 - color scale
+        colorScale.SetConditionCount(3);
+        colorScale.Criteria[0].FormatColorRGB = Color.FromArgb(230, 197, 218);
+        colorScale.Criteria[0].Type = ConditionValueType.LowestValue;
+        colorScale.Criteria[0].Value = "0";
+
+        colorScale.Criteria[1].FormatColorRGB = Color.FromArgb(244, 210, 178);
+        colorScale.Criteria[1].Type = ConditionValueType.Percentile;
+        colorScale.Criteria[1].Value = "50";
+
+        colorScale.Criteria[2].FormatColorRGB = Color.FromArgb(245, 247, 171);
+        colorScale.Criteria[2].Type = ConditionValueType.HighestValue;
+        colorScale.Criteria[2].Value = "100";
+         
+        //Saving the workbook
+        var outputStream = new FileStream("ConditionalFormat.xlsx", FileMode.Create, FileAccess.Write);
+        workbook.SaveAs(outputStream);
+
+        //Dispose streams
+        outputStream.Dispose();
+
+        var process = new Process();
+        process.StartInfo = new ProcessStartInfo("ConditionalFormat.xlsx")
+        {
+            UseShellExecute = true
+        };
+        process.Start();
+    }
+
+    private static void GenerateTimeSpan(IWorksheet worksheet, string column, TimeSpan start)
+    {
+        for (int i = 10; i < 20; i++)
+        {
+            var cell = worksheet[$"{column}{i}"];
+            start = start.Add(TimeSpan.FromMinutes(2));
+            cell.Text = start.ToString();
+        }
+    }
+}
