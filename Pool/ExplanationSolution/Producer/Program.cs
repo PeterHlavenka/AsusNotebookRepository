a07bece9064d95a08e6bf1c38c856d4fcca4fccb
PeterHlavenka <peter.hlavenka.consultant@nielsen.com>
2025-03-27 07:52:07 +0100

    RabbitMQ Producer, FirstConsumer, SecondConsumer
    v tomto stavu kdyz producer vyprodukuje message, tak ji sezere jen jeden consumer. Consumeri se stridaji v pojidani zprav

---
+++ Pool/ExplanationSolution/Producer/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+await using var connection = await factory.CreateConnectionAsync();
+await using var channel = await connection.CreateChannelAsync();
+
+await channel.QueueDeclareAsync(queue: "letterbox", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+var message = "Hello World!";
+
+var body = Encoding.UTF8.GetBytes(message);
+
+await channel.BasicPublishAsync(exchange: string.Empty, routingKey: "letterbox", body: body);
+
+Console.WriteLine($" [x] Sent {message}");
+Console.WriteLine("Prerequisites: Make sure RabbitMQ is running on localhost:5672");
+Console.WriteLine("You can show messages in the queue on http://localhost:8080/#/queues");
+Console.ReadLine();
---
+++ Pool/ExplanationSolution/SecondConsumer/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+await using var connection = await factory.CreateConnectionAsync();
+await using var channel = await connection.CreateChannelAsync();
+
+await channel.QueueDeclareAsync(queue: "letterbox", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+var consumer = new AsyncEventingBasicConsumer(channel);
+consumer.ReceivedAsync += async (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($" [x] Received {message}");
+    await Task.Delay(1000);
+};
+
+await channel.BasicConsumeAsync(queue: "letterbox", autoAck: true, consumer: consumer);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
+
+
+
+// v tomto stavu kdyz producer vyprodukuje message, tak ji sezere jen jeden consumer. Ktery to bude je nahodne.. 
