9bdaa0baba95b1ebc36d930ce771639ad6184946
PeterHlavenka <peter.hlavenka.consultant@nielsen.com>
2025-03-28 07:54:42 +0100

    predelano na MessageSendera z jineho projektu

--- Pool/ExplanationSolution/Producer/Program.cs
+++ Pool/ExplanationSolution/Producer/Program.cs
@@ -1,27 +1,10 @@
-﻿
+﻿using RabbitMQMessageSender;
 
-using System.Text;
-using RabbitMQ.Client;
 
-var factory = new ConnectionFactory() { HostName = "localhost" };
-await using var connection = await factory.CreateConnectionAsync();
-await using var channel = await connection.CreateChannelAsync();
+// producer uz nepotrebuje mit nainstalovany balik RabbitMQ.Client, protoze ho ma MessageSender
+var messageSender = new MessageSender();
+await messageSender.SendMessage("Hello World!");
 
-await channel.QueueDeclareAsync(queue: "persistent_Letterbox", durable: true, exclusive: false, autoDelete: false, arguments: null);
-
-var message = "Hello World!";
-
-var body = Encoding.UTF8.GetBytes(message);
-
-var properties = new BasicProperties
-{
-    Persistent = true
-};
-
-await channel.BasicPublishAsync(exchange: string.Empty, routingKey: "persistent_Letterbox", mandatory: true,
-    basicProperties: properties, body: body);
-
-Console.WriteLine($" [x] Sent {message}");
 Console.WriteLine("Prerequisites: Make sure RabbitMQ is running on localhost:5672");
 Console.WriteLine("You can show messages in the queue on http://localhost:8080/#/queues");
 Console.ReadLine();
---
+++ Pool/ExplanationSolution/RabbitMQMessageSender/MessageSender.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+namespace RabbitMQMessageSender;
+
+public class MessageSender
+{
+    public async Task SendMessage(string message)
+    {
+        var factory = new ConnectionFactory { HostName = "localhost" };
+        await using var connection = await factory.CreateConnectionAsync();
+        await using var channel = await connection.CreateChannelAsync();
+        await channel.QueueDeclareAsync("persistent_Letterbox", true, false, false, null);
+        
+        var body = Encoding.UTF8.GetBytes(message);
+        var properties = new BasicProperties
+        {
+            Persistent = true
+        };
+
+        await channel.BasicPublishAsync(string.Empty, "persistent_Letterbox", true,
+            properties, body);
+
+        Console.WriteLine($" [x] Sent {message}");
+    }
+}
