e9f21d0de38aed24223ff78fe3416f05283751a8
PeterHlavenka <peter.hlavenka.consultant@nielsen.com>
2025-04-02 07:12:12 +0200

    konecne tabulka se stejnymi sloupci

--- Pool/ExplanationSolution/DotVVM_table/Pages/Default/DefaultViewModel.cs
+++ Pool/ExplanationSolution/DotVVM_table/Pages/Default/DefaultViewModel.cs
@@ -11,24 +11,10 @@ public class DefaultViewModel : DotvvmViewModelBase
     public RabbitMqService RabbitMqService { get; set;}
 
     public string Title { get; set; }
-    public ObservableCollection<string> Messages => RabbitMqService.Messages;
     
-    public ObservableCollection<RabbitMessage> RabbitMessages => RabbitMqService.RabbitMessages;
-    
-    public string[] Items { get; set; } = { "First", "Second", "Third" };
-    public List<string> ItemsList { get; set; }
-
     public DefaultViewModel(RabbitMqService rabbitMqService)
     {
         RabbitMqService = rabbitMqService;
-        Title = "Hello from DotVVM!";
-        
-        // aby bylo mozne refreshovat stranku kdyz se zmeni ObservableCollection
-       // Messages = new ObservableCollection<string>();
-        //Messages.CollectionChanged += (_, _) => Context.RedirectToUrl(Context.HttpContext.Request.Path.ToString() ?? string.Empty);
-        
-        ItemsList = new List<string>(){ "ItemsList from constructor", "Second", "Third" };
-       // Messages.Add("V PreRenderu se mi to smaze");
-        ItemsList.Add("jak notifikovat");
+        Title = "RabbitMQ messages";
     }
 }
