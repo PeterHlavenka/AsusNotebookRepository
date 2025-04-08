2a017f7920990949c577ff55809dd8582575da83
PeterHlavenka <peter.hlavenka.consultant@nielsen.com>
2025-04-08 19:21:54 +0200

    rabbit sender extra

--- Pool/ExplanationSolution/ImportService/Program.cs
+++ Pool/ExplanationSolution/ImportService/Program.cs
@@ -3,6 +3,7 @@ using System.Text.Json;
 using ImportService;
 using RabbitCommon;
 using RabbitMQ.Client;
+using RabbitSender;
 
 // country, environment, dataType, service
 string[][] queues =
@@ -17,26 +18,19 @@ string[][] queues =
     // web is autmatically added in SendMessage
 ];
 
-var factory = new ConnectionFactory { HostName = "localhost" };
-await using var connection = await factory.CreateConnectionAsync();
-var channel = await connection.CreateChannelAsync();
+var neco = new RabbitMessageSender();
+await neco.Initialize(queues);
 
-// 1) Deklarujeme exchange
-var exchangeName = "importExchange";
-await channel.ExchangeDeclareAsync(exchangeName, ExchangeType.Topic, true, false);
-
-// 2) Deklarace vsech front a jejich binding na exchange:
-await DeclareAndBindQueues();
-
-
-await SendMultipleMessages();
+await neco.SendMessage("CZ", "Production", GenerateRandomDateTime(), [AdwDataIds.DataCzCsProTrend2, AdwDataIds.DataCzAdCross]);
 
 while (true)
 {
-    if (Console.ReadLine()?.ToLower() == "n")
-        await SendMessage("CZ", "Production", GenerateRandomDateTime(), [AdwDataIds.DataCzCsProTrend2]);
-    if (Console.ReadLine()?.ToLower() == "m")
-        await SendMultipleMessages();
+    var pismeno = Console.ReadLine();
+    if (pismeno == "m")
+        await SendMultipleMessages(neco);
+    
+    if (pismeno == "n")
+        await neco.SendMessage("CZ", "Production", GenerateRandomDateTime(), [AdwDataIds.DataCzCsProTrend2, AdwDataIds.DataCzAdCross]);
 }
 
 
@@ -44,7 +38,7 @@ DateTime GenerateRandomDateTime()
 {
     // generata random date and time between 2023-01-01 and 2023-12-31 
     var random = new Random();
-    var year = 2023;
+    var year = 2025;
     var month = random.Next(1, 13);
     var day = random.Next(1, DateTime.DaysInMonth(year, month) + 1);
     var hour = random.Next(0, 24);
@@ -53,49 +47,17 @@ DateTime GenerateRandomDateTime()
     return new DateTime(year, month, day, hour, minute, second);
 }
 
-async Task SendMessage(string country, string environment, DateTime importDateTime, string[] adwDataIds)
-{
-    // Send message to the exchange with routing key "import.{environment}.{country}"
-    var routingKey = $"{environment}.{country}";
-    var rabbitMessage = new RabbitMessage
-    {
-        Country = country,
-        Environment = environment,
-        DataTypes = adwDataIds,
-        ImportDate = importDateTime.ToString("yyyy-MM-dd HH:mm:ss")
-    };
-    var message = JsonSerializer.Serialize(rabbitMessage);
-    var body = Encoding.UTF8.GetBytes(message);
-    await channel.BasicPublishAsync(exchangeName, routingKey, body);
 
-    // a stejnou zpravu poslu na webovou stranku
-    var web = "webPage";
-    routingKey = $"{environment}.{country}.{web}";
-    Console.WriteLine("Sending message to web page");
-    await channel.BasicPublishAsync(exchangeName, routingKey, body);
-}
 
-async Task SendMultipleMessages()
+async Task SendMultipleMessages(RabbitMessageSender sender)
 {
     var dateTime = GenerateRandomDateTime();
     // CZ
-    await SendMessage("CZ", "Production", dateTime, [AdwDataIds.DataCzCsProTrend2, AdwDataIds.DataCzAdCross]);
-    await SendMessage("CZ", "Production", dateTime, [AdwDataIds.DataCzMrTvIndivid]);
-    await SendMessage("CZ", "Production", dateTime, [AdwDataIds.DataCzPemd]);
-    await SendMessage("CZ", "RC", dateTime, [AdwDataIds.DataCzAdCross]);
-// SK
-    await SendMessage("SK", "Production", dateTime, [AdwDataIds.DataSkKantarMonitoring]);
-    await SendMessage("SK", "RC", dateTime, [AdwDataIds.DataSkKantarTvIndivid]);
-}
-
-async Task DeclareAndBindQueues()
-{
-    foreach (var queue in queues)
-    {
-        var queueName = $"{queue[0]}_{queue[1]}_{queue[2]}Queue";
-        await channel.QueueDeclareAsync(queueName, true, false, false);
-        // v routovacim klici nebude definovany consumer (to je zname v deklaraci fronty). Message jde na vsechny consumery.
-        var routingKey = $"{queue[0]}.{queue[1]}";
-        await channel.QueueBindAsync(queueName, exchangeName, routingKey);
-    }
+    await sender.SendMessage("CZ", "Production", dateTime, [AdwDataIds.DataCzCsProTrend2, AdwDataIds.DataCzAdCross]);
+    await sender.SendMessage("CZ", "Production", dateTime, [AdwDataIds.DataCzMrTvIndivid]);
+    await sender.SendMessage("CZ", "Production", dateTime, [AdwDataIds.DataCzPemd]);
+    await sender.SendMessage("CZ", "RC", dateTime, [AdwDataIds.DataCzAdCross]);
+// neco.
+    await sender.SendMessage("SK", "Production", dateTime, [AdwDataIds.DataSkKantarMonitoring]);
+    await sender.SendMessage("SK", "RC", dateTime, [AdwDataIds.DataSkKantarTvIndivid]);
 }
