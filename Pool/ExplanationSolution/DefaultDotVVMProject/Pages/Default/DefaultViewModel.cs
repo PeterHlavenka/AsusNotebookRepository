a0205257f9553aa33316f3b875ee5fa153b818d5
PeterHlavenka <peter.hlavenka.consultant@nielsen.com>
2025-04-10 12:05:28 +0200

    pridani default aplikace v dotVVM - test jak se to bude chovat kdyz to zadockeruju

---
+++ Pool/ExplanationSolution/DefaultDotVVMProject/Pages/Default/DefaultViewModel.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using DotVVM.Framework.ViewModel;
+
+namespace DefaultDotVVMProject.ViewModels;
+
+public class DefaultViewModel : DotvvmViewModelBase
+{
+    public string Title { get; set; }
+
+    public DefaultViewModel()
+    {
+        Title = "Hello from DotVVM!";
+    }
+}
