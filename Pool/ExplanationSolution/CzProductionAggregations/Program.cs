275cab54d28f1854d352bfa0734ebb20166f2918
PeterHlavenka <peter.hlavenka.consultant@nielsen.com>
2025-04-28 09:29:35 +0200

    dosj

--- Pool/ExplanationSolution/CzProductionAggregations/Program.cs
+++ Pool/ExplanationSolution/CzProductionAggregations/Program.cs
@@ -1,6 +1,10 @@
 ﻿using Adwind.Rabbit.Messaging;
+using Microsoft.Extensions.Logging;
 
-var consumer = new RabbitMessageConsumer(DoSomeWork);
+const string connString = "amqp://phlavenka:LLykoat3J9HbDBUAjVW3@rmq.prod:5672/adw-test";
+var logger = LoggerFactory.Create(builder => builder.AddConsole()).CreateLogger<RabbitMessageConsumer>();
+var consumer = new RabbitMessageConsumer(connString, logger);
+consumer.Worker = DoSomeWork;
 await consumer.StartConsumingAsync(RabbitCommons.GetQueueInfo(ImportConsumer.Aggregations, "CZ", "Production"));
 
 Console.WriteLine("Consuming.");
